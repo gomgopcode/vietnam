0d50a9e176c7309b9a3931f5cdbdcc8078a29bb9
Gom Gop <94690791+gomgopcode@users.noreply.github.com>
2023-11-11 00:05:29 +0700

    Delete vndata.js

--- vndata.js
+++
@@ -1,9 +0,0 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", 'https://raw.githubusercontent.com/gomgopcode/vietnam/main/vietnam.json');
-xhr.onreadystatechange = function () {
-   if (xhr.readyState === 4) {
-  var VNdata = JSON.parse(xhr.responseText);
-
-   }
-}
-xhr.send();
